4a6e35e6e7af8817e8267ace44c5196ab8d47d95
AvishiGan <avishiganepola@gmail.com>
2023-07-13 11:22:19 +0530

    Added new dependencies to packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"];
+export const content = ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}", ];
 
 export const theme = {
   extend: {},
