22f4c060057708820ea188df82d2d06fdd64df9c
AvishiGan <avishiganepola@gmail.com>
2023-07-13 11:22:19 +0530

    Added new dependencies to packages

--- App.tsx
+++ App.tsx
@@ -1,31 +1,50 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import Welcome from "./src/app/Welcome";
-import RootNavigator from "./navigator/RootNavigator";
-import { NavigationContainer } from "@react-navigation/native";
-
-//custom fonts
-import AppLoading from "expo-app-loading";
-import { useFonts } from "expo-font";
-
-//React Navigation
-import RootStack from "../ufora/src/nav/RootStack";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import ProfileScreen from "./src/screens/profile/index";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    PoppinsRegular: require("./src/assets/fonts/Poppins-Regular.ttf"),
-    PoppinsMedium: require("./src/assets/fonts/Poppins-Medium.ttf"),
-    PoppinsBold: require("./src/assets/fonts/Poppins-Bold.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
   return (
-    // <RootStack />
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ProfileScreen />
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+// import { StatusBar } from "expo-status-bar";
+// import { Text, View } from "react-native";
+// import Welcome from "./src/app/Welcome";
+// import RootNavigator from "./navigator/RootNavigator";
+// import { NavigationContainer } from "@react-navigation/native";
+
+// //custom fonts
+// import AppLoading from "expo-app-loading";
+// import { useFonts } from "expo-font";
+
+// //React Navigation
+// import RootStack from "../ufora/src/nav/RootStack";
+
+// export default function App() {
+//   const [fontsLoaded] = useFonts({
+//     PoppinsRegular: require("./src/assets/fonts/Poppins-Regular.ttf"),
+//     PoppinsMedium: require("./src/assets/fonts/Poppins-Medium.ttf"),
+//     PoppinsBold: require("./src/assets/fonts/Poppins-Bold.ttf"),
+//   });
+
+//   if (!fontsLoaded) {
+//     return <AppLoading />;
+//   }
+
+//   return (
+//     // <RootStack />
+//     <NavigationContainer>
+//       <RootNavigator />
+//     </NavigationContainer>
+//   );
+// }
