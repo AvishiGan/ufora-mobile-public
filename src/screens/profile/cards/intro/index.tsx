39834b3182c40aa0d961fbb753a930292ac1654e
AvishiGan <avishiganepola@gmail.com>
2023-07-13 16:32:13 +0530

    Add all styles to intro card

--- src/screens/profile/cards/intro/index.tsx
+++ src/screens/profile/cards/intro/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Card, Button, Title, Paragraph } from "react-native-paper";
-import { styles } from "../styles";
+import { styles } from "./styles";
 
 const IntroCard: React.FC = () => {
   return (
     <Card style={styles.container}>
       <Card.Content>
-        <Title>Intro</Title>
+        <Title style={styles.title}>Intro</Title>
       </Card.Content>
       <Card.Content>
-        <Paragraph>
+        <Paragraph style={styles.paragraph}>
           Hey I am Isuru, an undergrad at UCSC. I love designing and creating
-          websites and posters... see more
+          websites and posters 
+          <Text style={styles.seeMore}>... see more</Text>
         </Paragraph>
       </Card.Content>
     </Card>
