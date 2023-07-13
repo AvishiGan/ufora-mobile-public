10ab7eb22bcbfef2cc5253d01beb501c0901b5bb
AvishiGan <avishiganepola@gmail.com>
2023-07-13 16:32:13 +0530

    Add all styles to intro card

--- src/screens/profile/cards/intro/styles.ts
+++ src/screens/profile/cards/intro/styles.ts
@@ -1,9 +1,50 @@
 import { StyleSheet } from "react-native";
-
+import { Paragraph } from "react-native-paper";
 
 export const styles = StyleSheet.create({
     container: {
-        alignItems: "center",
-        margin: 37,
+        display: 'flex',
+        width: 405,
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        gap: 10,
+        borderRadius: 15,
+        backgroundColor: '#FEFEFE'
     },
+    title: {
+        color: '#111',
+        leadingTrim: 'both',
+        textEdge: 'cap',
+        // fontFamily: 'Poppins',
+        fontSize: 18,
+        fontStyle: 'normal',
+        fontWeight: '600',
+        lineHeight: 24,
+    },
+    paragraph: {
+        width: 385,
+        // color: var(--brand-black, #111);
+        color: '#111',
+        leadingTrim: 'both',
+        textEdge: 'cap',
+        // fontFamily: 'Poppins',
+        fontSize: 14,
+        fontStyle: 'normal',
+        fontWeight: '400',
+        // line-height: 16px; /* 114.286% */
+        lineHeight: 16,
+    },
+    seeMore: {
+        // color: var(--brand-blue, #2656FF);
+        color: '#2656FF',
+        leadingTrim: 'both',
+        textEdge: 'cap',
+        // fontFamily: 'Poppins',
+        fontSize: 14,
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: 16,
+    }
 });
