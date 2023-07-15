40c5e862fa7e13ee5282f589ba4816880ad78cce
AvishiGan <avishiganepola@gmail.com>
2023-07-15 16:45:42 +0530

    Created clubData file which contains all information related to clubs

---
+++ src/screens/profile/cards/clubsAndSocieties/clubData.ts
@@ -0,0 +1,22 @@
+export const clubData = [
+    {
+      title: "PR Director at Rotaract Club of UCSC",
+      date: "2023-Present",
+    },
+    {
+      title: "Web team lead at IEEE Student Branch of UCSC",
+      date: "2023-Present",
+    },
+    {
+      title: "Member of Gavel Club of UOC",
+      date: "2022-2023",
+    },
+    {
+      title: "General Member of Rotaract Club of UCSC",
+      date: "2022-2023",
+    },
+    {
+      title: "Web team member at IEEE Student Branch of UCSC",
+      date: "2022-2023",
+    },
+  ];
