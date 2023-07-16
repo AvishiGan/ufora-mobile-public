8ce0596508fa16be593c67b6c2d5d217750ba247
AvishiGan <avishiganepola@gmail.com>
2023-07-16 19:32:55 +0530

    Separate the commonSection code into index.tsx and data.ts files

---
+++ src/screens/profile/commonSection/data.ts
@@ -0,0 +1,26 @@
+/**
+ * @interface ProfileData
+ *  Specify the properties and their types that the profileData object should have
+ */
+export interface ProfileData {
+    name: string;
+    university: string;
+    friendsCount: number;
+    postsCount: number;
+    blogsCount: number;
+    projectsCount: number;
+  }
+  
+  /**
+   * Instance of the ProfileData interface
+   * Object contain actual data
+   */
+  export const profileData: ProfileData = {
+    name: "Isuru Ranawaka",
+    university: "Undergrad at University of Colombo",
+    friendsCount: 45,
+    postsCount: 35,
+    blogsCount: 5,
+    projectsCount: 4,
+  };
+  
--- src/screens/profile/commonSection/index.tsx
+++ src/screens/profile/commonSection/index.tsx
@@ -1,40 +1,65 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Text } from "react-native";
 import { styles } from "./styles";
 import ProfileButton from "../../../components/profile/buttons/index";
+import { profileData } from "./data";
 
 const ProfileCommonSection: React.FC = () => {
+  /**
+   * @function handleButtonPress
+   * @returns void
+   * @description Handles the button press event
+   */
+  const handleButtonPress = () => {
+    console.log("Button pressed!");
+  };
+
   return (
-    <View style={styles.profileSection}>
-      {/* <Image source={require("./assets/images/profile/ProfilePicture.png")} /> */}
-      <Text style={styles.profileName}>Isuru Ranawaka</Text>
-      <Text style={styles.profileText}>Undergrad at University of Colombo</Text>
-      <View style={styles.horizontalContainer}>
-        <Text style={styles.profileText}>145 Friends</Text>
-        <Text style={styles.profileText}>35 Posts</Text>
-        <Text style={styles.profileText}>5 Blogs</Text>
-        <Text style={styles.profileText}>4 Projects</Text>
+    <View style={styles.infoContainer}>
+      <View style={styles.imageNameContainer}>
+        <View style={styles.nameUniContainer}>
+          <Text style={styles.profileName}>{profileData.name}</Text>
+          <Text style={styles.uniName}>{profileData.university}</Text>
+        </View>
       </View>
-      <View style={styles.horizontalContainer}>
+      <View style={styles.statusContainer}>
+        <View style={styles.individualStatusContainer}>
+          <StatusCount count={profileData.friendsCount} name="Friends" />
+          <StatusCount count={profileData.postsCount} name="Posts" />
+          <StatusCount count={profileData.blogsCount} name="Blogs" />
+          <StatusCount count={profileData.projectsCount} name="Projects" />
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
         <ProfileButton onPress={handleButtonPress} title="Edit Profile" />
         <ProfileButton onPress={handleButtonPress} title="Share Profile" />
-      </View>
-      <View style={styles.horizontalContainer}>
-        <ProfileButton onPress={handleButtonPress} title="About" />
-        <ProfileButton onPress={handleButtonPress} title="Posts" />
-        <ProfileButton onPress={handleButtonPress} title="Blogs" />
-        <ProfileButton onPress={handleButtonPress} title="Portfolio" />
       </View>
     </View>
   );
 };
 
 /**
- * This function is called when the button is pressed
+ * @interface StatusCountProps
+ * Defines the expected properties for the StatusCount component
  */
-const handleButtonPress = () => {
-  // Code logic to be executed when the button is pressed
-  console.log("Button pressed!");
+interface StatusCountProps {
+  count: number;
+  name: string;
+}
+
+/**
+ *
+ * @param param0 | StatusCountProps
+ * @returns | JSX.Element
+ * @description | Renders the count and name of a particular status
+ */
+const StatusCount: React.FC<StatusCountProps> = ({ count, name }) => {
+  return (
+    <Text style={styles.statusCount}>
+      {count}
+      <Text style={styles.statusName}> {name}</Text>
+    </Text>
+  );
 };
 
 export default ProfileCommonSection;
