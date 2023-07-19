6db20a76bbb0cb39e92f0655d84d08f06cd00ade
AvishiGan <avishiganepola@gmail.com>
2023-07-19 09:17:47 +0530

    Changed files to add svgs in files

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 import MyProfile from "./src/screens/profile";
 import SafeViewAndroid from "./src/components/SafeViewAndroid";
+import  LucideCake  from "./assets/LucideCake.svg";
 
 export default function App() {
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
       <ScrollView>
+        <LucideCake width={85} height={30} style={{ color: "#ccc" }} />
         <MyProfile />
       </ScrollView>
     </SafeAreaView>
--- declaration.d.ts
+++ declaration.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="nativewind/types" />
 
-declare module "*.svg" {
-  import React from 'react';
-  import { SvgProps } from "react-native-svg";
-  const content: React.FC<SvgProps>;
+declare module '*.svg' {
+  import {SvgProps} from 'react-native-svg';
+  const content: React.StatelessComponent<SvgProps>;
   export default content;
 }
 
 declare module "*.png";
 
 declare module "*.jpg"
+
