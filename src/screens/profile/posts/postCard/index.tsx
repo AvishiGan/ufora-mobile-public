271eea8760d54f2d879b30b1b2b165b6979147c0
AvishiGan <avishiganepola@gmail.com>
2023-07-22 13:42:12 +0530

    Created basic skeleton for posts in profile

--- src/screens/profile/posts/postCard/index.tsx
+++ src/screens/profile/posts/postCard/index.tsx
@@ -4,34 +4,45 @@ import { styles } from "./styles";
 import { Paragraph } from "react-native-paper";
 // import { ProfileHeaderData } from "./data";
 import ProfileHeader from "../../../../components/profile/profileHeader";
-import { architecture, thumbsUpIcon } from "../../../../../assets/images";
-// import { thumbsUpIcon } from "../../../../../assets/svgImages";
+import { architecture } from "../../../../../assets/images";
+import Svg, { Circle } from "react-native-svg";
+import { thumbsUpIcon } from "../../../../../assets/svgImages";
 
 const PostCard: React.FC = () => {
   return (
     <View style={styles.allPostsContainer}>
       <View style={styles.singlePostContainer}></View>
       {/* <ProfileHeader /> */}
-      <Paragraph style={styles.postDescription}>
+      <Text style={styles.postDescription}>
         This is an amazing architectural concept which we can implement in our
         group project
         <Text style={styles.seeMore}>... see more</Text>
-      </Paragraph>
+      </Text>
       <Image style={styles.postImage} source={architecture} />
       <View style={styles.feedbackContainer}>
         <View style={styles.likesCommentsContainer}>
           <View style={styles.likeButtonAndNamesContainer}>
-            <Image style={styles.likeButton} source={thumbsUpIcon} />
             {/* <thumbsUpIcon /> */}
             {/* Like button comes here */}
-            <Text style={styles.likedNames}>Kasun, Sumith</Text>
-            <Text style={styles.andOthers}> & 10 others</Text>
-            <Text style={styles.likedCount}>10 Likes</Text>
-            <View style={styles.likeShareCommentButtonContainer}>
-              <View style={styles.eachButtonContainer}></View>
-              {/* Each button comes here */}
-              <Text style={styles.eachButtonText}>Like</Text>
-            </View>
+            <Text style={styles.likedNames}>Kasun</Text>
+            <Text style={styles.andOthers}>& 100 others</Text>
+          </View>
+          <View style={styles.commentsCountContainer}>
+            <Text style={styles.commentsCountText}>50 comments</Text>
+          </View>
+        </View>
+        <View style={styles.likeShareCommentButtonContainer}>
+          <View style={styles.eachButtonContainer}>
+            {/* Each button comes here */}
+            <Text style={styles.eachButtonText}>Like</Text>
+          </View>
+          <View style={styles.eachButtonContainer}>
+            {/* Each button comes here */}
+            <Text style={styles.eachButtonText}>Comment</Text>
+          </View>
+          <View style={styles.eachButtonContainer}>
+            {/* Each button comes here */}
+            <Text style={styles.eachButtonText}>Share</Text>
           </View>
         </View>
       </View>
