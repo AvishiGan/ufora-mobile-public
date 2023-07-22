85e5f6f74464e9e91fbd5fcd7d9ab1cfa03b7f4d
AvishiGan <avishiganepola@gmail.com>
2023-07-22 18:41:45 +0530

    Added new images related to blog section on profile

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,4 +1,3 @@
-export {default as architecture} from './myProfile/posts/architecture.png';
-export {default as rugbyField} from './myProfile/posts/rugbyField.png';
-export {default as champions} from './myProfile/posts/champions.png';
-export {default as office} from './myProfile/posts/office.png';
+export * from '../images/myProfile/posts/index';
+export * from '../images/myProfile/blogs/index';
+
