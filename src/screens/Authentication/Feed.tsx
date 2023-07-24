afce124e11d2cb2bc775e7fc052c7bdaa2468ad8
Binuri01 <binuridesilva27@gmail.com>
2023-07-24 16:28:02 +0530

    testing axios

--- src/screens/Authentication/Feed.tsx
+++ src/screens/Authentication/Feed.tsx
@@ -23,21 +23,96 @@
 //     />
 //   </SafeAreaView>
 //   )
-  
 // };
-
 // export default Feed
 
-import React from "react";
-import { TextInput } from "@react-native-material/core";
 
-const Feed = () => (
-  <TextInput 
-    variant="outlined" 
-    label="Label" 
-    style={{ margin: 20}}
-  />
-);
+
+import React, {Component} from "react";
+import { TextInput } from "@react-native-material/core";
+import axios from 'axios';
+import {View, Text, SafeAreaView, Button} from 'react-native';
+
+
+const Feed = () => {
+  const callGetUsersList = () => {
+    axios
+      .get('https://crudcrud.com/api/be01073d24b24fd885964735d8457109/users')
+      .then(response =>{
+        console.log("Response:", response?.data);
+      }).catch(error => {
+        console.log("Error:",error);
+      })
+  };
+
+  const callGetUsersByIDList = (userID: string) => {
+    axios
+      .get(`https://crudcrud.com/api/be01073d24b24fd885964735d8457109/users/${userID}`)
+      .then(response =>{
+        console.log("Response:", response?.data);
+      }).catch(error => {
+        console.log("Error:",error);
+      })
+  };
+
+  const callAddUsers = () => {
+    let params = {userID:1, name: 'Avishi'};
+    axios
+      .post('https://crudcrud.com/api/be01073d24b24fd885964735d8457109/users',params)
+      .then(response =>{
+        console.log("Response:", response?.data)
+      }).catch(error => {
+        console.log("Error:",error);
+      })
+  }
+
+  const callUpdateUser = (userID: string) => {
+    let params = {userID:1, name: 'Avishi'};
+    axios
+      .put(
+        `https://crudcrud.com/api/be01073d24b24fd885964735d8457109/users/${userID}`,params,
+      )
+      .then(response =>{
+        console.log("Response:", response?.data);
+      }).catch(error => {
+        console.log("Error:",error);
+      })
+  }
+
+  const callDeleteUser = (userID: string) => {
+    axios
+      .delete(
+        `https://crudcrud.com/api/be01073d24b24fd885964735d8457109/users/${userID}`,
+      )
+      .then(response =>{
+        console.log("Response:", response?.data);
+      }).catch(error => {
+        console.log("Error:",error);
+      })
+  }
+
+  return(
+    <View>
+      <SafeAreaView>
+
+        <Button 
+          title={"Fetch Me"} 
+          onPress={() => {
+            callGetUsersList();
+          }}
+        />
+
+      <TextInput 
+        variant="outlined" 
+        label="Label" 
+        style={{ margin: 20}}
+      /> 
+      </SafeAreaView>
+    </View>
+  )
+}
+  
+  
 
 export default Feed;
 
