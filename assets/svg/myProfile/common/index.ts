5538a2211ed19d89fc4e47689ee0d04ecab85c40
AvishiGan <avishiganepola@gmail.com>
2023-07-26 15:59:47 +0530

    Exported all components which contain svg images into two index files

---
+++ assets/svg/myProfile/common/index.ts
@@ -0,0 +1,13 @@
+export { default as AddIcon } from './addIcon';
+export { default as BackIcon } from './backIcon';
+export { default as ChatIcon } from './chatIcon';
+export { default as DotIcon } from './dotIcon';
+export { default as EditIcon } from './editIcon';
+export { default as GlobeIcon } from './globeIcon';
+export { default as MoreIcon } from './moreIcon';
+export { default as SearchIcon } from './searchIcon';
+export { default as ShareIcon } from './shareIcon';
+export { default as SmallDotIcon } from './smallDotIcon';
+export { default as UforaIcon } from './uforaIcon';
+export { default as VerifiedIcon } from './verifiedIcon';
+
