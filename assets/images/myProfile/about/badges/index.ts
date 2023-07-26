9ec22b2d6751d40d9cc43cc1aa4b89aafaef3b72
AvishiGan <avishiganepola@gmail.com>
2023-07-26 21:58:06 +0530

    Added badges images in profile about section

--- assets/images/myProfile/about/badges/index.ts
+++ assets/images/myProfile/about/badges/index.ts
@@ -5,3 +5,4 @@ export {default as badge4} from './badge4.png';
 export {default as badge5} from './badge5.png';
 export {default as badge6} from './badge6.png';
 export {default as badge7} from './badge7.png';
+export {default as badge8} from './badge8.png';
