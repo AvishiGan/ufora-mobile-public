4c83098a0139b75c5cac4ca8150e61cfe5a5efc8
Binuri01 <binuridesilva27@gmail.com>
2023-07-27 22:32:49 +0530

    connect backend to login

--- src/screens/Authentication/Login.tsx
+++ src/screens/Authentication/Login.tsx
@@ -1,10 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Text,
-  View,
-  KeyboardAvoidingView,
-  StyleSheet
-} from "react-native";
+import { Text, View, KeyboardAvoidingView, StyleSheet } from "react-native";
 import logo from "../../../assets/logo.png";
 import RegularButton from "../../components/buttons/RegularButton";
 import { TouchableOpacity } from "react-native";
@@ -21,7 +16,8 @@ import { Field, Formik } from "formik";
 //navigation
 import { RootStackParamList } from "../../navigation/Nav/RootStack";
 import { StackScreenProps } from "@react-navigation/stack";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import axios from "axios";
 type Props = StackScreenProps<RootStackParamList, "Login">;
 
 interface FormValues {
@@ -30,133 +26,173 @@ interface FormValues {
 }
 
 const Login: FunctionComponent<Props> = ({ navigation }) => {
-    const initialValues: FormValues = {
-      email: "",
-      password: "",
-    };
+  const initialValues: FormValues = {
+    email: "",
+    password: "",
+  };
 
-    const handleLogin = (values: FormValues) => {
-      //const handleLogin = () => Alert.alert("Login");
-      // Making the API request
-      //console.log(values);
+  const handleLogin = async (values: FormValues) => {
+    try {
+      const response = await axios.post("http://192.168.181.71:3000/login",{
+        username: values.email,
+        password: values.password
+      });
+
+      console.log(values);
       navigation.navigate("Feed");
-      // navigation.navigate("Feed",
-      //     {email: JSON.stringify($email), 
-      //     password: JSON.stringify($password)}),
-    };
 
-    return (
-        <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <StatusBar />
-        <View>
-          {/* Top section */}
-          <Logo
-            source={logo}
-            mainText="Login"
-            subText="Login to your account "
-          />
+      console.log("API Response: ", response.data);
+    } catch (error: any) {
 
-          {/* Bottom section */}
-          <View style={{paddingHorizontal: 10, marginTop: 190, alignItems: "center"}}>
-            <Formik initialValues={initialValues} onSubmit={handleLogin}>
-              {({ handleChange, handleSubmit, values }) => (
-                <View>
+      if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.error("API error: ", error.response.data);
+        console.error("API error status: ", error.response.status);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("API error: No response received");
+        console.log(error);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("API error: ", error.message);
+      }
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar />
+      <View>
+        {/* Top section */}
+        <Logo source={logo} mainText="Login" subText="Login to your account " />
+
+        {/* Bottom section */}
+        <View
+          style={{
+            paddingHorizontal: 10,
+            marginTop: 190,
+            alignItems: "center",
+          }}
+        >
+          <Formik initialValues={initialValues} onSubmit={handleLogin}>
+            {({ handleChange, handleSubmit, values }) => (
+              <View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    backgroundColor: "transparent",
+                    borderRadius: 20,
+                    padding: 2,
+                  }}
+                >
+                  <Field
+                    component={InputField}
+                    imageSource={require("../../../assets/icons/user.png")}
+                    name="email"
+                    placeholder="Username / Email"
+                    onChangeText={handleChange("email")}
+                    value={values.email}
+                  />
+                </View>
+
+                <View
+                  style={{
+                    marginTop: 10,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    backgroundColor: "transparent",
+                    borderRadius: 20,
+                    padding: 2,
+                  }}
+                >
+                  <Field
+                    component={InputField}
+                    imageSource={require("../../../assets/icons/password.png")}
+                    name="password"
+                    placeholder="*********"
+                    secureTextEntry={true}
+                    onChangeText={handleChange("password")}
+                    value={values.password}
+                  />
+                </View>
+                <TouchableOpacity
+                  onPress={() => navigation.navigate("ForgotPassword")}
+                >
                   <View
                     style={{
                       flexDirection: "row",
-                      alignItems: "center",
-                      backgroundColor: "transparent",
-                      borderRadius: 20,
-                      padding: 2,
-                    }}
-                  >
-                    <Field
-                      component={InputField}
-                      imageSource={require("../../../assets/icons/user.png")}
-                      name="email"
-                      placeholder="Username / Email"
-                    />
-                  </View>
-
-                  <View
-                    style={{
+                      justifyContent: "flex-end",
                       marginTop: 10,
-                      flexDirection: "row",
-                      alignItems: "center",
-                      backgroundColor: "transparent",
-                      borderRadius: 20,
-                      padding: 2,
                     }}
                   >
-                    <Field
-                      component={InputField}
-                      imageSource={require("../../../assets/icons/password.png")}
-                      name="password"
-                      placeholder="*********"
-                      secureTextEntry={true}
-                    />
+                    <RegularSmall>
+                      <Text style={{ color: "#2656FF" }}>Forgot Password?</Text>
+                    </RegularSmall>
                   </View>
-                  <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
-                    <View
-                      style={{
-                        flexDirection: "row",
-                        justifyContent: "flex-end",
-                        marginTop: 10,
-                      }}
-                    >
-                      <RegularSmall>
-                        <Text style={{ color: "#2656FF" }}>
-                          Forgot Password?
-                        </Text>
-                      </RegularSmall>
-                    </View>
-                  </TouchableOpacity>
+                </TouchableOpacity>
 
-                  {/* Button */}
-                  <View
-                    style={{flexDirection: "row", marginTop: 25, alignItems: "center", width: 330, }} >
-                    <RegularButton onPress={handleSubmit}>
-                      <Text style={{ color: "#FEFEFE" }}>Log In</Text>
-                    </RegularButton>
-                  </View>
+                {/* Button */}
+                <View
+                  style={{
+                    flexDirection: "row",
+                    marginTop: 25,
+                    alignItems: "center",
+                    width: 330,
+                  }}
+                >
+                  <RegularButton onPress={handleSubmit}>
+                    <Text style={{ color: "#FEFEFE" }}>Log In</Text>
+                  </RegularButton>
                 </View>
-              )}
-            </Formik>
+              </View>
+            )}
+          </Formik>
 
-            <View style={{ marginTop: 20, alignItems: "center", flexDirection: "row", }} >
-              <RegularNormal>
-                <Text style={{ alignItems: "center" }}>Don't have an account?</Text>
-              </RegularNormal>
-              <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
-                <View>
-                  <RegularNormal>
-                    <Text style={{ color: "#2656FF" }}>Create Account</Text>
-                  </RegularNormal>
-                </View>
-              </TouchableOpacity>
-            </View>
+          <View
+            style={{
+              marginTop: 20,
+              alignItems: "center",
+              flexDirection: "row",
+            }}
+          >
+            <RegularNormal>
+              <Text style={{ alignItems: "center" }}>
+                Don't have an account?
+              </Text>
+            </RegularNormal>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("CreateAccount")}
+            >
+              <View>
+                <RegularNormal>
+                  <Text style={{ color: "#2656FF" }}>Create Account</Text>
+                </RegularNormal>
+              </View>
+            </TouchableOpacity>
           </View>
-          <Authentication
-            onPressGoogle={handlePressGoogle}
-            onPressApple={handlePressApple}
-          />
         </View>
-        </KeyboardAvoidingView>
-    );
+        <Authentication
+          onPressGoogle={handlePressGoogle}
+          onPressApple={handlePressApple}
+        />
+      </View>
+    </KeyboardAvoidingView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    input: {
-        width: '80%',
-        height: 40,
-        borderWidth: 1,
-        borderColor: 'gray',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
 });
 
-export default Login;
+export default Login;
