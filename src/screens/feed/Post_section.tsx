887b60654b3b07c91e64ed7114fe6006195acc7e
devshehan <shehaan.avishka00@gmail.com>
2023-08-01 14:12:20 +0530

    implemented Post_section.tsx and Story_section.tsx section implemented

---
+++ src/screens/feed/Post_section.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View} from 'react-native'
+
+
+// COMPONENTS
+// import PostCard from '../../components/Post/PostCard'
+
+const Post_section = () => {
+  return (
+    <View>
+      {/* <PostCard/>
+      <PostCard/>
+      <PostCard/>
+      <PostCard/> */}
+    </View>
+  )
+}
+
+export default Post_section
