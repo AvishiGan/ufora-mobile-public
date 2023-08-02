0a358847ad522fc7dc0d90e97d1c5f3a9b9384e3
AvishiGan <avishiganepola@gmail.com>
2023-08-02 20:59:12 +0530

    Made a common Native stylesheet. Restructured folders.

--- src/components/profile/createPost/sub-components/buttons/styles.ts
+++ src/components/profile/createPost/sub-components/buttons/styles.ts
@@ -3,14 +3,14 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   buttonContainer: {
     display: "flex",
-    padding: 5,
-    paddingHorizontal: 10,
-    justifyContent: "center",
+    padding: 10,
+    paddingHorizontal: 20,
+    // justifyContent: "center",
     alignItems: "center",
     gap: 10,
-    borderRadius: 5,
+    borderRadius: 10,
     borderWidth: 1,
-    borderColor: "#4F4E4E",
+    borderColor: "#4F4E4",
     flexDirection: "row",
   },
   buttonContent: {
