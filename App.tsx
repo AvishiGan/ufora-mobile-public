f55a6838cce928fad6706650d298501c2be4e4a8
AvishiGan <avishiganepola@gmail.com>
2023-08-02 20:59:12 +0530

    Made a common Native stylesheet. Restructured folders.

--- App.tsx
+++ App.tsx
@@ -1,51 +1,16 @@
-// // import React from "react";
-// // import { View, StyleSheet, ScrollView } from "react-native";
-// // import MyProfile from "./src/screens/profile";
-
-// // export default function App() {
-// //   return (
-// //     <ScrollView>
-// //       <MyProfile />
-// //     </ScrollView>
-// //   );
-// // }
-
-// import { StatusBar } from "expo-status-bar";
-// import { Text, View } from "react-native";
-// // import Welcome from "./src/screens/Authentication/Welcome";
-// import RootNavigator from "./src/navigation/Nav/RootStack";
-// import { NavigationContainer } from "@react-navigation/native";
-
-// //custom fonts
-// import AppLoading from "expo-app-loading";
-// import { useFonts } from "expo-font";
-
-// //React Navigation
-// import RootStack from "./src/navigation/Nav/RootStack";
-
-// export default function App() {
-//   const [fontsLoaded] = useFonts({
-//     "Text": require("./assets/fonts/Switzer-Variable.ttf"),
-//     "ItalicText": require("./assets/fonts/Switzer-VariableItalic.ttf"),
-//   });
-
-//   if (!fontsLoaded) {
-//     return <AppLoading />;
-//   }
-
-//   return (
-//     <> 
-//     <RootStack />
-//     </>
-//   );
-// }
-
-
-// //////////////////////////////////////////////////////////////////////////
-// SHEHAN ENTRY
-
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+// import Welcome from "./src/screens/Authentication/Welcome";
+import RootNavigator from "./src/navigation/Nav/RootStack";
 import { NavigationContainer } from "@react-navigation/native";
-import RootNavigator from "./src/navigation/Navigator/RootNavigator";
+
+//custom fonts
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
+
+//React Navigation
+import RootStack from "./src/navigation/Nav/RootStack";
+import ProfileStack from "./routes/profileStack";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -58,8 +23,23 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-       <RootNavigator/>
-    </NavigationContainer>
+    <>
+      {/* <RootStack /> */}
+      <ProfileStack />
+    </>
   );
 }
+
+// // //////////////////////////////////////////////////////////////////////////
+// // SHEHAN ENTRY
+
+// import { NavigationContainer } from "@react-navigation/native";
+// import RootNavigator from "./src/navigation/Navigator/RootNavigator";
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//        <RootNavigator/>
+//     </NavigationContainer>
+//   );
+// }
