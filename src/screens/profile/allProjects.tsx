58ac410eb1d199d7d3a29efabb15cac95844c865
AvishiGan <avishiganepola@gmail.com>
2023-08-03 22:05:05 +0530

    Added SafeAreaView to each screen

--- src/screens/profile/allProjects.tsx
+++ src/screens/profile/allProjects.tsx
@@ -8,32 +8,35 @@ import { mobileApp, hostel, robotic } from "../../../assets/images";
 import { styles } from "./styles";
 import ContentAdditionBar from "../../components/profile/contentAdditionBar";
 import ProfileCommonSection from "./commonSection";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfilePortfolioSection = () => {
   return (
-    <View>
-      <ProfileCommonSection />
-      <View style={styles.projectActionContainer}>
-        <ContentAdditionBar title="Add a new project" />
-        <ProjectCard
-          projectImage={mobileApp}
-          projectTitle="University Social Media App"
-          projectDescription="Introducing University Social Media App, designed to revolutionize campus connections and foster an engaging online community for students and faculty alike."
-        />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <ProfileCommonSection />
+        <View style={styles.projectActionContainer}>
+          <ContentAdditionBar title="Add a new project" />
+          <ProjectCard
+            projectImage={mobileApp}
+            projectTitle="University Social Media App"
+            projectDescription="Introducing University Social Media App, designed to revolutionize campus connections and foster an engaging online community for students and faculty alike."
+          />
 
-        <ProjectCard
-          projectImage={hostel}
-          projectTitle="Hostel Booking & Management System"
-          projectDescription="Welcome to advanced Hostel Booking & Management System, streamlining accommodations and ensuring seamless experiences for users"
-        />
+          <ProjectCard
+            projectImage={hostel}
+            projectTitle="Hostel Booking & Management System"
+            projectDescription="Welcome to advanced Hostel Booking & Management System, streamlining accommodations and ensuring seamless experiences for users"
+          />
 
-        <ProjectCard
-          projectImage={robotic}
-          projectTitle="Robotics & IOT"
-          projectDescription="Unlock the potential of the future with our Robotics & IoT innovations."
-        />
+          <ProjectCard
+            projectImage={robotic}
+            projectTitle="Robotics & IOT"
+            projectDescription="Unlock the potential of the future with our Robotics & IoT innovations."
+          />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
