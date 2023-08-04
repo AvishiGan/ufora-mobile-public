9ec2ec6ff48cdddbe170a7949aeba8c27f9d2111
AvishiGan <avishiganepola@gmail.com>
2023-08-04 06:26:30 +0530

    Added common section to profile about section

--- src/screens/profile/about/index.tsx
+++ src/screens/profile/about/index.tsx
@@ -3,13 +3,16 @@ import { View } from "react-native";
 import AboutCardSection from "./cards";
 import { ProfileSlider } from "../../../components";
 import ProfileCommonSection from "../commonSection";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfileAboutSection: React.FC = () => {
   return (
-    <View>
-      {/* <ProfileCommonSection /> */}
-      <AboutCardSection />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <ProfileCommonSection />
+        <AboutCardSection />
+      </View>
+    </SafeAreaView>
   );
 };
 
