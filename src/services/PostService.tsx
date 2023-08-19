ba0a9aaa7705ae986805d57bb183d6c7b20eb638
Avishi Ganepola <69346466+AvishiGan@users.noreply.github.com>
2023-08-19 08:28:07 +0530

    Completed frontend backend connecting logic to create profile both undergrad and company

--- src/services/PostService.tsx
+++ src/services/PostService.tsx
@@ -2,31 +2,26 @@ import ApiClient from "../util/ApiClient";
 import { AccessLevel } from "../util";
 import { Post } from "../model";
 
-/**
- * @param caption | Caption for the post.
- * @param content | Content (image or other media) for the post
- */
 export const createPost = async (post: Post) => {
   try {
     const response = await ApiClient.post("/post/create", post);
     console.log("Post created:", response);
     alert("Post created!");
   } catch (error) {
-    // console.error("Error creating post:", error.message);
     throw error;
   }
 };
 
 /**
- *
  * @returns | Returns a list of posts
  */
 export const getPosts = async () => {
   try {
-    const response = await ApiClient.get<Post[]>("api/post"); // Assuming Post is the type for your post objects
+    // Assuming Post is the type for the post objects
+    const response = await ApiClient.get<Post[]>("api/post");
     console.log("Posts retrieved:", response);
     return response;
   } catch (error) {
-    // Handle errors here
+    throw error;
   }
 };
--- src/util/ApiClient.tsx
+++ src/util/ApiClient.tsx
@@ -1,46 +1,36 @@
 import axios, { AxiosResponse } from "axios";
 import { HOST_NAME } from "./constants/hostName";
 
-/**
- * @template T | Generic type parameter
- */
 interface ApiResponse<T> {
   data: T;
 }
 
-/**
- * @constant httpClient | Create a reusable instance of axios with common configurations
- */
 const httpClient = axios.create({
   baseURL: HOST_NAME,
   headers: {
     "Content-Type": "application/json",
-    Authorization:
-      // Remove this manually added token
-      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNWZ5OTJ1Y2g2OWk2OHVkd2Z5dGEiLCJ1c2VyX3R5cGUiOiJ1bmRlcmdyYWR1YXRlIiwiaWF0IjoxNjkyMTI1MDg4LCJleHAiOjE2OTQ3MTcwODgsInVzZXJuYW1lIjpudWxsfQ.yinLBy2sx0gAW5jvWJrv_EFfnvUShA0ylKckUNWGhU0",
+    Authorization: "Bearer YOUR_ACCESS_TOKEN",
   },
 });
 
 const ApiClient = {
-  post: async <T,>(url: string, data: any): Promise<T> => {
+  post: async <T,>(url: string, data: any): Promise<ApiResponse<T>> => {
     try {
       const response = await httpClient.post<ApiResponse<T>>(url, data);
-      return response.data.data;
+      return response.data;
     } catch (error) {
-      // Handle errors here
       throw error;
     }
   },
 
-  get: async <T,>(url: string): Promise<T> => {
+  get: async <T,>(url: string): Promise<ApiResponse<T>> => {
     try {
       const response = await httpClient.get<ApiResponse<T>>(url);
       return response.data;
     } catch (error) {
-      // Handle errors here
       throw error;
     }
   },
 };
 
-export default ApiClient;
+export default ApiClient;
