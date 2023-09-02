fa259bcfe58650623d11880eb744ee27894c6001
Avishi Ganepola <69346466+AvishiGan@users.noreply.github.com>
2023-09-02 21:24:20 +0530

    Created a new util named fileUtils

---
+++ src/util/fileUtils.ts
@@ -0,0 +1,29 @@
+import {
+  launchImageLibraryAsync,
+  ImagePickerAsset,
+  MediaTypeOptions,
+} from "expo-image-picker";
+import { FileData } from "../model";
+
+export const selectMediaFromDevice = async (
+  mediaCount: number
+): Promise<FileData[]> => {
+  try {
+    let { assets, canceled } = await launchImageLibraryAsync({
+      mediaTypes: MediaTypeOptions.All,
+      allowsEditing: true,
+      base64: true,
+      selectionLimit: mediaCount,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!canceled && assets)
+      return Promise.resolve(
+        assets.map((asset: ImagePickerAsset) => new FileData(asset))
+      );
+    else return Promise.resolve([]);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
